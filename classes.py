d63465ed9c7c08b148a5dc5f6645d41ba5aa04cc
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2020-12-11 17:22:07 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,26 +7,32 @@ from config import fp_articles, fp_ref, article_auteurs, auteur_articles, articl
 
 data=pd.read_csv(f'{article_auteurs}.csv',sep=',',encoding='utf-32',usecols=['id_article','auteurs'],index_col='id_article')
 data2=pd.read_csv(f'{auteur_articles}.csv',sep=',',encoding='utf-32',usecols=['auteur','references'],index_col='auteur')
-ref=pd.read_csv(f'{article_ref}.csv',sep=',',encoding='utf-32',usecols=['id_article'],index_col='id_article')
-data.sort_values(by='id_article',axis=0,inplace=True)
-ref.sort_values(by='id_article',axis=0,inplace=True) 
-"""
-class Article:
+ref=pd.read_csv(f'{article_ref}.csv',sep=',',encoding='utf-32',usecols=['id_article','references'],index_col='id_article')
+
+class Article: #OK
+	"""
+	Un article = un id + une liste d'auteurs
+	"""
+
 	def __init__(self,given_id):
 		self.id=int(given_id)
-		self.ref=ref.ref_id[self.id]
-		self.authors=data.Authors[self.id]
-"""
+		self.ref=ref.references[self.id]
+		self.auteurs=data.auteurs[self.id]
+
 class Auteur:
 	def __init__(self,name):
 		self.name=name
-		self.list_articles=data.query('self.name in data.Authors')
+		self.list_articles=data.query("@name in data.auteurs")
+		print(data.auteurs)
 
-"""		
+
+	
 class Communaute:
+
 	def __init__(self,auteur,profondeur):
 		self.auteur=auteur
 		self.profondeur=profondeur
-"""
+
+
 Itzy=Auteur('C. Itzykson')
 print(Itzy.name,Itzy.list_articles) 
