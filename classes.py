c650f1e49139d546bc2b263c856330f9e9ec039c
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2020-12-11 16:20:05 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -5,28 +5,28 @@ import os
 from collections import defaultdict
 from config import fp_articles, fp_ref, article_auteurs, auteur_articles, article_ref
 
-data=pd.read_csv(f'{article_auteurs}',sep=',',usecols=['paper_id','Authors'],index_col='paper_id')
-data2=pd.read_csv(f'{auteur_articles}',sep=',',usecols=['paper_id','Authors'],index_col='paper_id')
-ref=pd.read_csv(f'{auteur_articles}',sep=',',usecols=['paper_id','ref_id'],index_col='paper_id')
-data.sort_values(by='paper_id',axis=0,inplace=True)
-ref.sort_values(by='paper_id',axis=0,inplace=True) 
-
+data=pd.read_csv(f'{article_auteurs}.csv',sep=',',encoding='utf-32',usecols=['id_article','auteurs'],index_col='id_article')
+data2=pd.read_csv(f'{auteur_articles}.csv',sep=',',encoding='utf-32',usecols=['auteur','references'],index_col='auteur')
+ref=pd.read_csv(f'{article_ref}.csv',sep=',',encoding='utf-32',usecols=['id_article'],index_col='id_article')
+data.sort_values(by='id_article',axis=0,inplace=True)
+ref.sort_values(by='id_article',axis=0,inplace=True) 
+"""
 class Article:
-    def __init__(self,given_id):
-        self.id=int(given_id)
-        self.ref=ref.ref_id[self.id]
-        self.authors=data.Authors[self.id]
+	def __init__(self,given_id):
+		self.id=int(given_id)
+		self.ref=ref.ref_id[self.id]
+		self.authors=data.Authors[self.id]
+"""
+class Auteur:
+	def __init__(self,name):
+		self.name=name
+		self.list_articles=data.query('self.name in data.Authors')
 
-class Author:
-    def __init__(self,name):
-        self.name=name
-        self.list_articles=data.query('self.name in data.Authors')
-
-		
+"""		
 class Communaute:
 	def __init__(self,auteur,profondeur):
 		self.auteur=auteur
-        self.profondeur=profondeur
-
-
-
+		self.profondeur=profondeur
+"""
+Itzy=Auteur('C. Itzykson')
+print(Itzy.name,Itzy.list_articles) 
