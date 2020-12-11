b83df8e762abb2f740111309399ac230f64897ed
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2020-12-11 13:27:07 +0100

    Retrait de '| |' dans le split

--- config.py
+++ config.py
@@ -5,13 +5,7 @@ fp_articles = r'C:\Users\lepau\OneDrive\Desktop'
 # chemin d'accès aux dossier contenant les articles
 fp_ref = r'C:\Users\lepau\OneDrive\Desktop'
 
-# nom du fichier crée après le pre-processing du dossier articles
+# nom des fichiers créés après le pre-processing
 nom_articles1 = "resume_articles"
 nom_articles2 = "auteur_ref"
-
-
-# nom du fichier crée après le pre-processing du dossier articles
 nom_references = "reference_articles"
-
-# => nomage bof pour les deux dernières variables
-# => essayer d'en trouver d'autres
