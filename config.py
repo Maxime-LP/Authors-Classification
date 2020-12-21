fc818323820a5326815f7e36ac43890e2695d40b
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2020-12-21 20:24:52 +0100

    d

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # chemin d'accès aux dossier contenant les articles
-fp_articles = r'/home/amait/Documents/python/POO-Projet/'
-# Maxime : r'C:\Users\lepau\OneDrive\Desktop'
-# Adrien : r'/home/amait/Documents/python/POO-Projet/'
-
+#fp_articles = r'/home/amait/Documents/python/POO-Projet/'
+#fp_articles = r'C:\Users\lepau\OneDrive\Desktop'
+fp_articles = r'C:\Users\Utilisateur\Desktop'
 # chemin d'accès aux dossier contenant les articles
-fp_ref = r'/home/amait/Documents/python/POO-Projet/'
-# Maxime : r'C:\Users\lepau\OneDrive\Desktop'
-# Adrien : r'/home/amait/Documents/python/POO-Projet/'
-
+#fp_ref = r'/home/amait/Documents/python/POO-Projet/'
+#fp_ref = r'C:\Users\lepau\OneDrive\Desktop'
+fp_ref = r'C:\Users\Utilisateur\Desktop'
 # nom des fichiers créés après le pre-processing
 article_auteurs = "df_p"
 auteur_articles = "df_a"
