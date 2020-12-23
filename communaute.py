f3f3c99ccc62fba805e668c1f256665c460c689e
amait <amait41>
2020-12-23 06:43:23 +0100

    vérif fonction cite => ok + ajout fichier pour paratge d'idées

--- communaute.py
+++ communaute.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 		pre_processing(sys.argv[2], sys.argv[3])
 
 	elif sys.argv[1] == 'cite':
-		Auteur(sys.argv[2]).cite(sys.argv[3])
+		print(Auteur(sys.argv[2]).cite(sys.argv[3]))
 
 	elif sys.argv[1] == 'influences':
 		print(Auteur(sys.argv[2]).influences(sys.argv[3]))
