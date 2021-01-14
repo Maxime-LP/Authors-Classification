825fa05f2e0f7807a0d828e504b70fb495b35734
amait <amait41>
2021-01-14 13:33:16 +0100

    exceptions rétablies

--- communaute.py
+++ communaute.py
@@ -20,7 +20,7 @@ except IndexError:
 	print('Argument(s) invalide(s).')
 	sys.exit()
 except FileNotFoundError:
-	print('Veuillez fournir des données à l\'aide de la commande init.')
+	print('Données manquantes. Veuillez fournir des données à l\'aide de la commande init.')
 	print('Pour plus d\'informations taper \'./communaute aide\'')
 	sys.exit()
 #############################################
@@ -54,30 +54,35 @@ def aide():
 if __name__ == "__main__":
 
 	# selection d'une commande		
-	#try:
-	if sys.argv[1]=='aide':
-		aide()
+	try:
+		if sys.argv[1]=='aide':
+			aide()
 
-	elif sys.argv[1] == 'init':
-		pre_processing(sys.argv[2], sys.argv[3])
+		elif sys.argv[1] == 'init':
+			pre_processing(sys.argv[2], sys.argv[3])
 
-	elif sys.argv[1] == 'cite':
-		res = Auteur(sys.argv[2]).cite(sys.argv[3])
-		# tri des valeurs du dictionnaire
-		res = sorted(res.items(), key=lambda t: t[1])
-		# affichage avec le module pretty print
-		pp.pprint(res)
+		elif sys.argv[1] == 'cite':
+			res = Auteur(sys.argv[2]).cite(sys.argv[3])
+			# tri des valeurs du dictionnaire
+			res = sorted(res.items(), key=lambda t: t[1])
+			# affichage avec le module pretty print
+			pp.pprint(res)
 
-	elif sys.argv[1] == 'est_cite':
-		res = Auteur(sys.argv[2]).est_cite(sys.argv[3])
-		res = sorted(res.items(), key=lambda t: t[1])
-		pp.pprint(res)
+		elif sys.argv[1] == 'est_cite':
+			res = Auteur(sys.argv[2]).est_cite(sys.argv[3])
+			res = sorted(res.items(), key=lambda t: t[1])
+			pp.pprint(res)
 
-	elif sys.argv[1] == 'communaute1':
-		Communaute(sys.argv[2],sys.argv[3]).graph_simple()
+		elif sys.argv[1] == 'communaute1':
+			Communaute(sys.argv[2],sys.argv[3]).graph_simple()
 
-	elif sys.argv[1] == 'communaute2':
-		Communaute(sys.argv[2],sys.argv[3]).graph_relations()
+		elif sys.argv[1] == 'communaute2':
+			Communaute(sys.argv[2],sys.argv[3]).graph_relations()
 
-	else:
-		raise KeyError
+		else:
+			raise KeyError
+
+	except KeyError:
+		print(f'La commande \'{sys.argv[1]}\' n\'est pas valide. Pour plus d\'informations taper \'./communaute aide\'')
+	except IndexError:
+		print('Argument(s) invalide(s).') 
